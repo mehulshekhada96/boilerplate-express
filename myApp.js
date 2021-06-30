6df1e20c74a8b04491e515ff88d530a0ef727146
Mehul Shekhada <mehulshekhada96@gmail.com>
2021-06-30 14:54:24 +0530

    Practice of Requests

--- myApp.js
+++ myApp.js
@@ -1,25 +1,45 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser') 
 require('dotenv').config()
 
 
 // console.log("Hello World")
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/views/index.html' )
   })
   
   app.use('/public', express.static(__dirname + '/public'))
-  const mySecret = process.env.MESSAGE_STYLE
-  let response = mySecret==='uppercase'?{message: "Hello json".toUpperCase()}:{message: "Hello json"}
 
+
+  app.route("/name").post(function(req, res) {
+   
+    var { first: firstName, last: lastName } = req.body;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
+
+//   app.get('/:word/echo', (req, res)=>{
+//       res.send({echo : req.params.word})
+//   })
+//   app.use('/', (req,res,next)=>{
+//       console.log(`${req.method} ${req.path} - ${req.ip}`)
+//       next();
+//   })
   
-  console.log(response)
-  app.get('/json',(req,res)=>{
-    res.send(response)
+//   app.get('/now',(req,res,next)=>{
+//     req.time =  new Date().toString()  
+//     next();
+//   },(req,res)=>{
+//       res.send({time: req.time})
+//   })
   
-  
-  })
+//   app.get('/json',(req,res)=>{
+//     res.send(process.env.MESSAGE_STYLE==='uppercase'?{message: "Hello json".toUpperCase()}:{message: "Hello json"})
+//   })
   
 
  module.exports = app;
